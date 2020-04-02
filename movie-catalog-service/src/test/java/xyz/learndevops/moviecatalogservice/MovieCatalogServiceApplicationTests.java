539acd4c4bc4a0a8cc4edeca20a543749a7c5a4f
atishukla <atishayshukla@gmail.com>
2020-04-02 17:48:25 +0200

    Initial projects

---
+++ movie-catalog-service/src/test/java/xyz/learndevops/moviecatalogservice/MovieCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.learndevops.moviecatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
